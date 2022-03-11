95142a702736614a44d824a1caf4e9aef694ebfe
felix abongo <aggreyfelix@yahoo.com>
2022-03-10 22:16:43 -0800

    second commit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 # Using Puppet, install puppet-lint
 
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem',
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
